5106029467319a5b286b069d9e4c26fcf743fc62
KEPENGQI <53080849+KEPENGQI@users.noreply.github.com>
2020-06-11 14:32:17 +0800

    Update view.php

--- view.php
+++ view.php
@@ -1,3 +1,11 @@
 <?php
- lalalallala
+   $connectionInfo = array("UID" => "kepengqi", "pwd" => "Wal3300418", "Database" => "tp046981", "LoginTimeout" => 30, 
+                           "Encrypt" => 1, "TrustServerCertificate" => 0);
+   $serverName = "tcp:tp046981.database.windows.net,1433";
+   $conn = sqlsrv_connect($serverName, $connectionInfo);
+ if(!$conn)
+  {
+    die("Error connection: ".sqlsrv_errors());
+  }
+  echo "Connection Success: connected!";
 ?>
